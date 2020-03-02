edb4aaef4575c483ba89ce49d98923910b8d9628
nilsnilk <40207496+nilsnilk@users.noreply.github.com>
2020-03-02 15:35:07 +0100

    Update js-functions.js

--- js-functions.js
+++ js-functions.js
@@ -1,20 +1,27 @@
 function toggleShowcase(id) {
 	let element = document.getElementById(id);
+	let body = document.getElementById("_body");
 	if(element.classList) {
 		element.classList.toggle("hidden");
+		body.classList.toggle("unscrollable");
 	} else {
 		let classes = element.className.split(" ");
+		let body_classes = body.className.split(" ");
 		let i = classes.indexOf("hidden")
-		if(i >= 0) {
+		let j = body_classes.indexOf("unscrollable");
+		if(i >= 0 && j < 0) {
 			classes.splice(i, 0);
+			body_classes.push("unscrollable");
 		} else {
+			body_classes.splice(j, 0);
 			classes.push("hidden");
-			element.className = classes.join(" "); 
+			element.className = classes.join(" ");
 		}
 	}
 }
 function closeShowCase() {
 	let elements = document.getElementsByClassName("modal");
+	document.getElementById("_body").className = "";
 	if(elements.classList) {
 		for(let i = 0; i < elements.length; i++) {
 			elements[i].classList.add("hidden");
@@ -28,5 +35,6 @@ function closeShowCase() {
 				elements[i].className = classes.join(" ");
 			}
 		}
+		
 	}
 }
